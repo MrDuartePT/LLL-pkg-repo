53688055e2fd80b21f9ade53a37a767be71ec9d7
Gonçalo Duarte <gonegrier.duarte@gmail.com>
2023-08-25 17:17:37 +0100

    fix ci

--- subprojects/darkdetect_deb/build_rpm_deb.sh
+++ subprojects/darkdetect_deb/build_rpm_deb.sh
@@ -3,7 +3,7 @@ BUILD_DIR=/tmp/deb_darkdetect
 
 set -ex
 #Intsall debian packages
-sudo apt-get install debhelper dkms python3-all python3-stdeb dh-python
+sudo apt-get install debhelper dkms python3-all python3-stdeb dh-python alien
 
 # recreate BUILD_DIR
 rm -rf "${BUILD_DIR}" || true
